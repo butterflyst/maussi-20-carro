29e0203962355be76b8f4e7e8c45abc1d85c1854
butterflyst <147355245+butterflyst@users.noreply.github.com>
2023-10-08 20:17:09 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+    	
+    } else if (receivedNumber == 2) {
+    	
+    } else if (receivedNumber == 3) {
+    	
+    } else if (receivedNumber == 4) {
+    	
+    }
+})
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
